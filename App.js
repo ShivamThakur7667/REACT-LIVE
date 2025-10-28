f034a8334c630b0a6844188a5e6ef29c73373c80
ShivamThakur7667 <shivamthakur766793@gmail.com>
2025-10-29 02:32:49 +0530

    lecture 02

--- App.js
+++ App.js
@@ -1,8 +1,25 @@
+import React from "react";
+import ReactDom from "react-dom/client";
+
 const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "h1 tag"),
-    React.createElement("h2", {}, "h2 tag"),
-  ]),
+  React.createElement(
+    "div",
+    { id: "child1", key: "child1" },
+
+    [
+      React.createElement("h1", { key: "a" }, "h1 tag"),
+      React.createElement("h2", { key: "b" }, "h2 tag"),
+    ]
+  ),
+  React.createElement(
+    "div",
+    { id: "child2", key: "child2" },
+
+    [
+      React.createElement("h1", { key: "c" }, "h1 tag"),
+      React.createElement("h2", { key: "d" }, "h2 tag"),
+    ]
+  ),
 ]);
 
 console.log(parent);
